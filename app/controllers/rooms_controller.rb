7eaa23711605f3dd607e9e2a6c558485d8a61203
Ee Kin <wongeekinn@gmail.com>
2024-04-24 23:50:25 +0800

    feat: creating models controllers for room

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,50 @@
+class RoomsController < ApplicationController
+    before_action :set_room, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @rooms = Room.all
+    end
+
+    def show
+    end
+
+    def new
+       @room = Room.new
+    end
+
+    def create
+       @room = Room.new(room_params)
+
+       if @room.save
+           redirect_to @room, notice: "Room was successfully created."
+       else
+           render :new
+       end
+    end
+
+    def edit
+    end
+
+    def update
+        if @room.update(room_params)
+            redirect_to @room, notice: "Room was successfully updated."
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @room.destroy
+        redirect_to rooms_url, notice: "Room was successfully destroyed."
+    end
+
+    private
+
+    def set_room
+        @room = Room.find(params[:id])
+    end
+
+    def room_params
+        params.require(:room).permit(:name)
+    end
+end
