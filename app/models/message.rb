74ec578088e95feb1093b527a11b5bdaeffeb83a
Ee Kin <wongeekinn@gmail.com>
2024-04-28 00:44:07 +0800

    add annotate gem and room message relationship

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,22 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :bigint           not null, primary key
+#  content    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  room_id    :bigint           not null
+#
+# Indexes
+#
+#  index_messages_on_room_id  (room_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (room_id => rooms.id)
+#
 class Message < ApplicationRecord
+    belongs_to :room
+    validates :content, presence: true
 end
--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :bigint           not null, primary key
+#  content    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  room_id    :bigint           not null
+#
+# Indexes
+#
+#  index_messages_on_room_id  (room_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (room_id => rooms.id)
+#
 require "test_helper"
 
 class MessageTest < ActiveSupport::TestCase
